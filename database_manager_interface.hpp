c96e66b38b8ea1f3bd5b1d7706da40e26add3e10
MaximKanevskiy <145599322+MaximKanevskiy@users.noreply.github.com>
2024-06-23 12:07:23 +0300

    added reference to string city name to find last snapshots of cpecific city

--- database_manager_interface.hpp
+++ database_manager_interface.hpp
@@ -5,6 +5,6 @@ class IDB_Manager
 {
 public:
 	virtual void Write(std::unique_ptr<WeatherData> weatherData) = 0;
-	virtual WeatherData ReadLast() = 0;
+	virtual WeatherData ReadLast(const std::string& city) = 0;
 	virtual std::string GetKey() = 0;
 };
