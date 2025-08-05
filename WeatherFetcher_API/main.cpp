f427d5de0225481fe223c2524b3a555a4fdb8699
MaximKanevskiy <maxkanevskiy430@yandex.ru>
2025-08-05 12:45:16 +0300

    refactor(api): add default config values if configuration is debug

--- WeatherFetcher_API/main.cpp
+++ WeatherFetcher_API/main.cpp
@@ -3,21 +3,30 @@
 
 int main(int argc, char** argv)
 {
+#ifndef DEBUG
     if (argc < 5)
     {
         std::cerr << "Usage:\n\tapi <db_host> <user> <password_file> <schema>" << std::endl;
         return 1;
     }
+#endif
 
     std::string serverhost = "tcp://0.0.0.0:5555";
 
     try
     {
         std::unordered_map<std::string, std::string> config;
-        config["host"] = argv[1];
-        config["user"] = argv[2];
+#ifndef DEBUG
+        config["host"]          = argv[1];
+        config["user"]          = argv[2];
         config["password_file"] = argv[3];
-        config["schema"] = argv[4];
+        config["schema"]        = argv[4];
+#else
+        config["host"]          = "tcp://localhost:3306";
+        config["user"]          = "root";
+        config["password_file"] = "password.txt";
+        config["schema"]        = "weather";
+#endif
 
         auto api = std::make_unique<Z_API>(serverhost, config);
         api->Run();
